f0aa394a8086453b9a7603dcda3d6a7010872c6c
usersrc <websphaere@cryptolab.net>
2016-10-04 03:55:22 +0200

    Create a contact page form that submits to a controller action and add a responsive burger menu to the navigation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,11 @@ class StaticPagesController < ApplicationController
   def redirect_me
     redirect_to "/static_pages/landing_page"
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
   get 'static_pages/index'
-
   get 'static_pages/landing_page'
+  
+  post 'static_pages/thank_you'
 
   # root 'static_pages#landing_page'
   root 'static_pages#redirect_me'
