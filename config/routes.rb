1625b44e8c3e7d07340a5c06d83e84ccfdb0732c
usersrc <websphaere@cryptolab.net>
2016-10-06 02:48:10 +0200

    Create comments controller and view and implement rating plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
-  resources :users
-  resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+  # resources :products
+  resources :products do
+    resources :comments
+  end
+  resources :users
 
   get 'static_pages/about'
   get 'static_pages/contact'
