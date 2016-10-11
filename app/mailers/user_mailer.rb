c8804442582c32c4c627c745f0cad627ef79e0ae
usersrc <websphaere@cryptolab.net>
2016-10-11 03:31:18 +0200

    Fix navbar behaviour and add automated email on sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
       :to => 'railsendnow@re-gister.com',
       :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "Bike Shop"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
+  end
 end
