46ee5842f5f345753f6fa5f4d1ed14d60d9a0a85
usersrc <websphaere@cryptolab.net>
2016-10-12 08:06:12 +0200

    Create and set up payment controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'static_pages/landing_page'
 
   post 'static_pages/thank_you'
+  post 'static_pages/payment'
 
   root 'static_pages#landing_page'
   # root 'static_pages#redirect_me'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010232802) do
+ActiveRecord::Schema.define(version: 20161012053210) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 20161010232802) do
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "colour"
-    t.float    "price"
+    t.integer  "price_in_cents"
   end
 
   create_table "users", force: :cascade do |t|
