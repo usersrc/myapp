1ec25c2dbdda0fabd6113d5c3083c7390b8f4c5f
usersrc <websphaere@cryptolab.net>
2016-10-31 13:24:24 +0100

    Add function to fade out new comment alert

--- app/assets/javascripts/notices.js
+++ app/assets/javascripts/notices.js
@@ -1,4 +1,10 @@
-// 
+//
 // $( document ).ready(function() {
 //     $(".alert").fadeIn("slow").delay(2500).fadeOut(1000);
 // });
+
+
+var fadeOutNewCommentAlert = function() {
+  console.log("test");
+  $(".comment-alert").delay(800).fadeOut(5000);
+}
